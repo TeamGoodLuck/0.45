e36195f5b9ea0860042828879ef73190d631717a
김창겸 <kyum8562@naver.com>
2023-09-03 20:39:06 +0900

    :art: Create loginUI
    create loginUI

--- fe/lib/main.dart
+++ fe/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fe/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,8 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: LogIn(),
+    );
   }
 }
